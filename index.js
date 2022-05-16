81d4fc533957cd2cec4ba8edfe6102e837db3106
ramin929 <102694314+ramin929@users.noreply.github.com>
2022-05-16 19:01:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,36 +22,6 @@ return number % 2 === 0 ? true : false;
 };
 console.log(getResult(prompt('Введите число!'))); 
 
-//task5
-const deleteChars = function (str) {
-return `${str.slice(1, -1)}`;
-};
-console.log(deleteChars(prompt('Введите строку!'))); */
-
-//task6
-const newString = function (str) {
-return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
-};
-console.log(newString(prompt('Введите строку!'))); 
-
-//task7
-const showMassage = function (letter) {
-return letter === letter.toUpperCase() ? 'Оууу май, большая буква!' : 'Нет уж, маленькие буквы - скучно';
-};
-console.log(showMassage(prompt('Введите букву!'))); 
-
-//task8
-let stringOne = String(prompt('Введит первую строку!'));
-let stringTwo = String(prompt('Введите вторую строку!'));
-console.log(`${stringOne} ${stringTwo}`); 
-
-
-//task9
-const getResult = function (string, number) {
-return string.length > number ? 'String is too long!' : string;
-};
-console.log(getResult(prompt('Введите строку!'), prompt('Введите число!')));  
-
 
 
 
